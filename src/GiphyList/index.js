4f0c082e7ceae5950b5cb80ddfab1f811c6d6d55
AnnaKisel <anyakisel17@gmail.com>
2019-08-10 16:44:28 +0300

    almost fixed fetch, but it sends!

--- src/GiphyList/index.js
+++ src/GiphyList/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {List} from 'immutable';
+import fromJS, {List} from 'immutable';
 
 import style from './index.css';
 
@@ -13,12 +13,15 @@ class GiphyList extends React.Component {
     }
 
     handleChange = (el) => {
-        // fetch(`api.giphy.com/v1/gifs/search/q=${ el.target.value }&key=Yo75O065RqJlF1cCZ12LRNMFKWC2psky`).then((l) => {
-        //     this.setState({
-        //         // value: el.target.value,
-        //         list: fromJS(l)
-        //     });
-        // });
+        fetch(`api.giphy.com/v1/gifs/search/q=${ el.target.value }&key=Yo75O065RqJlF1cCZ12LRNMFKWC2psky`)
+            .then(response => response.json())
+            .then((list) => {
+                console.log(list);
+                this.setState({
+                    // value: el.target.value,
+                    list: fromJS(list)
+                });
+        });
         this.setState({value: el.target.value});
     };
 
